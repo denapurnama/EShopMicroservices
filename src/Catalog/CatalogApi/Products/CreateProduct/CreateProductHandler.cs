d7f5d4f1fe14033e27994729c63785028921dbda
denapurnama4@gmail.com <denapurnama4@gmail.com>
2025-11-19 19:49:31 +0700

    Create feature handler class with CQRS and MediatR

---
+++ src/Catalog/CatalogApi/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace CatalogApi.Products.CreateProduct;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    : IRequest<CreaProductResult>;
+public record CreaProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreaProductResult>
+{
+    public Task<CreaProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
