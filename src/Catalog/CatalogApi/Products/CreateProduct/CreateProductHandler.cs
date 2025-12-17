c7c60966ebb597432c674e4bc4ea134afb511dff
denapurnama4@gmail.com <denapurnama4@gmail.com>
2025-12-17 20:18:09 +0700

    Update product haandler

--- src/Catalog/CatalogApi/Products/CreateProduct/CreateProductHandler.cs
+++ src/Catalog/CatalogApi/Products/CreateProduct/CreateProductHandler.cs
@@ -1,14 +1,18 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
 
 namespace CatalogApi.Products.CreateProduct;
 public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
-    : IRequest<CreaProductResult>;
-public record CreaProductResult(Guid Id);
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreaProductResult>
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreaProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        // create product logic here
+        // save to database
+        // return created product result
+
         throw new NotImplementedException();
     }
 }
